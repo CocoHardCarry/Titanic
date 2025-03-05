aaf8acdc9f2e15f623d51974a0894dea9ee1543b
EmpresCoco <jaydenhang08@gmail.com>
2025-03-04 17:46:04 -0800

    Add input

--- Input.py
+++ Input.py
@@ -1,14 +1,29 @@
 import streamlit as st
+import pandas as pd
+from fastai.tabular.all import *
+
+st.title("Titanic Survivorship")
+
+titanic_model = load_learner("titanic_model.pkl")
 
 name = st.text_input("What is your name:")
 age = st.number_input("How old are you:")
-gender = st.selectbox("What is your gender:", ["male", "female"])
-price = st.number_input("Enter your ticket price:")
-size = st.number_input("Enter the number of people in your party:")
+sex = st.radio("What is your gender:", ("male", "female"))
+fare = st.number_input("Enter your ticket price:")
+sib_sp= st.number_input("Enter the number of people in your party:")
+parch = st.number_input("Enter Parent of Children: ")
+pclass = st.number_input("Passenger class: ")
 
 if st.button("Submit"):
-    st.write(f"Name: {name}")
-    st.write(f"Age: {age}")
-    st.write(f"Gender: {gender}")
-    st.write(f"Ticket Price: {price}")
-    st.write(f"Party Size: {size}")
+    user_input = {
+        "Age": [age],
+        "Sex": [sex],
+        "Fare": [fare],
+        "SibSp": [sib_sp],
+        "Parch": [parch],
+        "Pclass": [pclass]
+    }
+
+new_df = pd.DataFrame(user_input)
+prediction = titanic_model.predict(new_df.iloc[0])
+st.title(f"{name}, the chance you will survive Titanic disaster would be: {prediction[2][1] * 100}%")
